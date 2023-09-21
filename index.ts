4ba4ad03765fe304cfb65db6627adbd9f82daefe
Charles Morin <charles.mor@outlook.fr>
2023-09-21 15:54:10 -0400

    Updated code for newest substreams-sink version

--- index.ts
+++ index.ts
@@ -2,10 +2,8 @@ import { setup, http } from "substreams-sink";
 
 import { type ActionOptions } from "./bin/cli.js";
 
-import pkg from "./package.json" assert { type: "json" };
-
 export async function action(options: ActionOptions) {
-    const { emitter } = await setup(options, pkg);
+    const { emitter } = await setup(options);
     emitter.on("anyMessage", message => {
         // Do something with the message
         console.log(message);
